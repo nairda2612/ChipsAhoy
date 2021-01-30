f17c9afb3543c32a9a57dca2735bcbbb2b88fc95
nairda2612 <adrianpererads@gmai.com>
2021-01-30 13:12:55 +0100

    Linking R script to bash script

--- peaks_script.R
+++ peaks_script.R
@@ -1,27 +1,49 @@
+## Author: The Winx Club
+## Email: winx@alum.alfea.es
+## Date: January 30
 
-## Installing packages
+##This script works with arguments in order to submit it with R script. 
+##If you want to run it in Rstudio, don't use arguments. ARREGLAR!!!
 
+# Arguments:
+arguments <- commandArgs(trailingOnly = TRUE)
+
+txdb <- arguments[1] #Total genes of AT
+narrow_peak <- arguments[2] #Peaks file
+summit_peak <- arguments[3] #Summits file
+# posible argumento para up y down stream COMENTAR
+annoDb_name <- arguments[4] #Anotación de AT
+annoDb_param <- arguments[5]
+up1 <- as.numeric(arguments[6])
+down1 <- as.numeric(arguments[7])
+up2 <- as.numeric(arguments[8])
+down2 <- as.numeric(arguments[9])
+maintitle <- as.numeric(arguments[10])
+
+
+## Installing packages:
 
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 
-BiocManager::install("ChIPseeker")
+if (!require("ChIPseeker")) BiocManager::install("ChIPseeker")
 library("ChIPseeker")
 
 # paketes de organismos concretos
 
-BiocManager::install("TxDb.Athaliana.BioMart.plantsmart28")
-library("TxDb.Athaliana.BioMart.plantsmart28")
+if (!require(txdb)) BiocManager::install(txdb)
+library(txdb)
 
-BiocManager::install("org.At.tair.db")
-library("org.At.tair.db")
-
-BiocManager::install("clusterProfiler")
+if (!require("clusterProfiler")) BiocManager::install("clusterProfiler")
 library("clusterProfiler")
+
+if (!require(annoDb_name)) BiocManager::install(annoDb_name)
+library(annoDb_name)
+
 
 ## Getting information from organism 
 
-txdb <- TxDb.Athaliana.BioMart.plantsmart28
+####txdb <- TxDb.Athaliana.BioMart.plantsmart28
 
 ## Getting genetic information from organism
 
@@ -41,13 +63,14 @@ my.universe<-genes.chr1$gene_id
 
 ## Reading peaks
 
-peak <- readPeakFile(peakfile = "1_peaks.narrowPeak")
+peak <- readPeakFile(peakfile = narrow_peak)
 peak
+#ACORDARSE QUE LOS NARROW IBAN EN COMILLA EN LOS ARGUMENTOS
+## Reading summits, maximo de los picos
 
-## Reading cumbres, maximo de los picos
-
-peak2 <- readPeakFile(peakfile = "1_summits.bed")
+peak2 <- readPeakFile(peakfile = summit_peak)
 peak2 
+#ACORDARSE QUE LOS SUMMIT IBAN EN COMILLA EN LOS ARGUMENTOS
 
 ## En covplot es la quinta columna, normalmente "V5" aunque mi ordenador no,
 ## por eso he puesto yo X211, pq asi se llama en mi caso
@@ -58,20 +81,21 @@ covplot(peak, weightCol="X211")
 
 ## Defining promoters
 
-Promotores_peak <- getPromoters(TxDb=txdb, upstream=1000, downstream=0)
+Promoters_peak <- getPromoters(TxDb=txdb, upstream=up1, downstream=down1)
 
-Promotores_peak2 <- getPromoters(TxDb=txdb, upstream=1000, downstream=1000)
+Promoters_peak2 <- getPromoters(TxDb=txdb, upstream=up2, downstream=down2)
 
 ## Peak annotation
 
-help("annotatePeak")
+#help("annotatePeak")
 
 #asegurar para cual se hace
 #peakAnno_peak <- annotatePeak(peak = peak, tssRegion = c(-1000,0), TxDb = txdb, annoDb = "org.At.tair.db" )
 #plotAnnoPie(peakAnno)
 
-peakAnno_peak2 <- annotatePeak(peak = peak2, tssRegion = c(-1000,1000), TxDb = txdb, annoDb = "org.At.tair.db" )
+peakAnno_peak2 <- annotatePeak(peak = peak2, tssRegion = c(-up2,down2), TxDb = txdb, annoDb = annoDb_name)
 plotAnnoPie(peakAnno)
+
 
 df.annotation <- as.data.frame(peakAnno_peak)
 head(df.annotation)
@@ -99,7 +123,7 @@ length(prr5.regulome)
 
 ego <- enrichGO(gene          = prr5.regulome,
                 universe      = my.universe,
-                OrgDb         = org.At.tair.db,
+                OrgDb         = annoDb_param,
                 ont           = "BP",
                 keyType = 'TAIR')
 head(ego)
@@ -109,25 +133,79 @@ head(ego)
 ## que se supone q me tiene q salir al hacer head(ego?) a el le salen 30
 ## enriched terms found y a mi 256 xd 
 ## y los plots a partir de aki me he keao atras
+#Abro un canal para generar el archivo png
+
+
+png(file=maintitle[1],
+    width     = 10,
+    height    = 10,
+    units     = "in",
+    res       = 600
+)
+
+## Genero la imagen 
 
 barplot(ego, showCategory=20)
+##Cierro el canal
+dev.off()
+
+
 
 ## le sale que regula respuesta a carriquina pero a mi no. es un
 ## compuesto de incendio, ayuda a germinacion despues de incendio. 
 ## germinacion tras incendio es dependiente del reloj circadiano y 
 ## especificamente de prr5.
 
+png(file=maintitle,
+    width     = 10,
+    height    = 10,
+    units     = "in",
+    res       = 600
+)
+
+## Genero la imagen 
+
 dotplot(ego)
+##Cierro el canal
+dev.off()
+
 
 ## A el le gusta el siguiente, que es una red de concepto. Une genes
 ## a terminos de GO
+
+png(file=maintitle,
+    width     = 10,
+    height    = 10,
+    units     = "in",
+    res       = 600
+)
+
+## Genero la imagen 
+
 cnetplot(ego)
+##Cierro el canal
+dev.off()
+
+
 
 
 ## Tb esta bien porque dice que prr5 esta involucrado en desarrollo de
 ## organos (a mi no me sale) y en respuestas a falta de agua, sust org,
 ## y luego de forma aislada a carriquina y frio
+png(file=maintitle,
+    width     = 10,
+    height    = 10,
+    units     = "in",
+    res       = 600
+)
+
+## Genero la imagen 
+
 emapplot(ego)
+##Cierro el canal
+dev.off()
+
+
 
 ## Otro tipo de enriquecimiento que no hemos visto es el de las rutas
 ## KEGG (mirar por nuestra cuenta). 
